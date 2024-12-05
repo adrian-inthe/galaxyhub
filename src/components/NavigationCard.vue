b845e1634e592b4d846e7da83ff74c2a506ff365
Adrian <>
2024-12-05 17:01:34 +0100

    Add navigation

---
+++ src/components/NavigationCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <Card class="cursor-pointer hover:shadow-lg">
+    <template #title>
+      <div class="flex flex-col items-center">
+        <div class="flex flex-col items-center">
+          <img
+            :src="`/${iconSrc}`"
+            alt="Resource icon"
+            class="w-12 sm:w-16 lg:w-20 mb-4"
+          />
+          <h2
+            class="text-2xl text-gray-700 dark:text-white font-semibold uppercase"
+          >
+            {{ title }}
+          </h2>
+        </div>
+      </div>
+    </template>
+    <template #content>
+      <p class="m-0 text-gray-500 text-center">{{ description }}</p>
+    </template>
+  </Card>
+</template>
+
+<script lang="ts" setup>
+import Card from "primevue/card";
+
+defineProps({
+  iconSrc: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,4 +12,25 @@
     <span class="font-sf">Star Wars</span>
     <span class="uppercase"> universe!</span>
   </p>
+  <nav class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-10">
+    <NavigationCard
+      description="Meet the heroes, villains, and all the legends in between."
+      iconSrc="boba-fett.png"
+      title="Characters"
+    />
+    <NavigationCard
+      description="Explore iconic worlds from Tatooine to Coruscant!"
+      iconSrc="planet.png"
+      title="Planets"
+    />
+    <NavigationCard
+      description="Zoom through the galaxy in the fastest ships known to the Force."
+      iconSrc="x-wing.png"
+      title="Starships"
+    />
+  </nav>
 </template>
+
+<script async lang="ts" setup>
+import NavigationCard from "../components/NavigationCard.vue";
+</script>
