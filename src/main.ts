af66c0ae3fc2f56908e4ca47acb0c0d7a4daa0af
Adrian <>
2024-12-05 15:45:34 +0100

    Bootstrap Vue project

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import "primeicons/primeicons.css";
+import "./index.css";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+app.mount("#app");
